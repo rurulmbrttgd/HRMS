ab0c01f9dca7ca398f0a4ee1174ec4c2ff7d2d4d
rurulmbrttgd <ronaldlambertnicolastugadi@gmail.com>
2023-09-11 13:03:22 +0800

    first commit

---
+++ api/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import second from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+con.connect(function(err) {
+    if(err) {
+        console.log("Error in Connection");
+    } else {
+        console.log("Connected");
+    }
+})
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+    con.query(sql, [req.body.email, req.body.password], (err, result) => {
+        if(err)
+            return 
+            res.json({Status: "Error!"});
+        if(result.length > 0) {
+            return res.json({Success: "Success!"});
+        } else {
+            return res.json({Status: "Error!"});
+        }
+    })
+})
+app.listen(8081, () => {
+    console.log("Running");
+})
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import './style.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+function Login() {
+
+    const [values, setValues] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8081/login', values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+
+    return (
+        <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+            <div className='p-3 rounded w-25 border loginForm'>
+                <div className="text-center"> {/* Added a container for the image and centered it */}
+                    <img
+                        src="https://res.cloudinary.com/dxzvh2xex/image/upload/v1692943459/capstone-logo_rj4krr.png"
+                        alt=""
+                        className="width-172px logo"
+                    />
+                </div>
+                <h2 className='text-center'>Capstone-Intel Corporation</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="username"><strong>Username</strong></label>
+                        <input
+                            type="username"
+                            placeholder="Email or Username"
+                            name="username"
+                            className="form-control rounded-20"
+                            onChange={e => setValues({...values, username: e.target.value})}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            className="form-control rounded-20"
+                            onChange={e => setValues({...values, password: e.target.value})}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-20">Log in</button>
+                    <p>You agree to our terms and conditions</p>
+                    {/* <button className="btn btn-default border w-100 bg-light rounded-20">
+                        Create Account
+                    </button> */}
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
