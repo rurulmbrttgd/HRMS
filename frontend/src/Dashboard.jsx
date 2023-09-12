352edfa0a00995645adbe25c3f8368b8a5fa7bdb
Paul <johnpaul.c.edit@gmail.com>
2023-09-12 13:59:34 +0800

    push ni lester

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -99,8 +99,6 @@ function Dashboard() {
                 <div class="col p-0 m-0">
                     <div className='p-2 d-flex justify-content-left shadow topbar banner'>
                     <h1 className='p-3 h1text'>DASHBOARD</h1>
-					
-
                     </div>
                 <Outlet />
                 </div>
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import './style.css'
+import Calendar from 'react-calendar';
+
+const ReactCalendar = () => {
+    const [date, setDate] = useState(new Date());
+  
+    const onChange = date => {
+      setDate(date);
+    };
+  
+    return (
+      <div>
+        <Calendar showWeekNumbers onChange={onChange} value={date} />
+        {console.log(date)}
+        {date.toString()}
+      </div>
+    );
+  };
+  
+function Home() {
+  const [adminCount, setAdminCount] = useState()
+  const [employeeCount, setEmployeeCount] = useState()
+  const [salary, setSalary] = useState()
+
+  useEffect(() => {
+    axios.get('http://localhost:8081/adminCount')
+		.then(res => {
+			setAdminCount(res.data[0].admin)
+		}).catch(err => console.log(err));
+
+    axios.get('http://localhost:8081/employeeCount')
+		.then(res => {
+			setEmployeeCount(res.data[0].employee)
+		}).catch(err => console.log(err));
+
+    axios.get('http://localhost:8081/salary')
+		.then(res => {
+			setSalary(res.data[0].sumOfSalary)
+		}).catch(err => console.log(err));
+
+  } , [])
+    return (
+        <div className='d-flex'>
+        <div className='d-flex justify-content-around mt-3'>
+            <div className='px-3 pt-2 pb-3 department'>
+            <div className='text-center pb-1'>
+                <h4>Departments</h4>
+            </div>
+            <div className=''>
+                <h5>Total: {adminCount}</h5>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <div className='no-employees'>
+                <h5>100 Employees</h5>
+                </div>
+                <div className='no-interns'>
+                <h5>30 Interns</h5>
+                </div>
+                <div className='no-staff'>
+                <h5>20 Staff</h5>
+                </div>
+            </div>
+            </div>
+
+            <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+            <Calendar /> {/* Insert the Calendar component here */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
