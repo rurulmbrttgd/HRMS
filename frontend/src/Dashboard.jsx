2c93a75a5f3f6dfd9a3e78de04f18f2eb9aabbee
Paul <johnpaul.c.edit@gmail.com>
2023-09-12 14:41:17 +0800

    lester

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -30,41 +30,46 @@ function Dashboard() {
 		}).catch(err => console.log(err));
 	}
 	return (
-		<div className="container-fluid">
-			<div className="row flex-nowrap">
-				<div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-					<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-						<a href="/" className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
-							<span className="fs-5 fw-bolder d-none d-sm-inline">Admin Dashboard</span>
-						</a>
-						<ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-							<li>
-								<Link to="/" data-bs-toggle="collapse" className="nav-link text-white px-0 align-middle">
-									<i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </Link>
-							</li>
-							<li>
-								<Link to="/employee" className="nav-link px-0 align-middle text-white">
-									<i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Manage Employees</span> </Link>
-							</li>
-							<li>
-								<Link to="profile" className="nav-link px-0 align-middle text-white">
-									<i className="fs-4 bi-person"></i> <span className="ms-1 d-none d-sm-inline">Profile</span></Link>
-							</li>
-							<li onClick={handleLogout}>
-								<a href="#" className="nav-link px-0 align-middle text-white">
-									<i className="fs-4 bi-power"></i> <span className="ms-1 d-none d-sm-inline">Logout</span></a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="col p-0 m-0">
-					<div className='p-2 d-flex justify-content-center shadow banner'>
-						<h4>Employee Management System</h4>						
-					</div>
-					<Outlet />
-				</div>
-			</div>
-		</div>
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 sidebar">
+                <div className="d-flex flex-column align-items-center justify-content-center px-3 pt-2 text-white min-vh-100 text-center">
+                    <a href="/" className="d-flex-direction-row align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
+                        <img src="/public/Images/Capstone-logo.png" alt="HRMS Logo" className="sidebarlogo" />
+                        <span className="fs-5 fw-bolder d-none d-sm-inline align-item">Capstone-Intel HR Management System</span>
+                    </a>
+
+                    <ul className="nav nav-pills flex-column  mb-sm-auto mb-0 align-items-center align-items-sm-start"  id="menu" style={{ marginTop: '150px' }}>
+                    <li>
+                        <Link to="/" data-bs-toggle="collapse" className="nav-link text-white px-0 align-middle">
+                        <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/employee" className="nav-link px-0 align-middle text-white">
+                        <i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Employees</span>
+                        </Link>
+                    </li>
+               
+                    </ul>
+                    {/* Move the logout button here */}
+                    <li onClick={handleLogout}>
+                    <a href="#" className="nav-link px-0 align-middle text-white">
+                        <i className="fs-4 bi-power"></i> <span className="ms-1 d-none d-sm-inline">Logout</span>
+                    </a>
+                    </li>
+                </div>
+                </div>
+
+                <div class="col p-0 m-0">
+                    <div className='p-2 d-flex justify-content-left shadow topbar banner'>
+                    <h1 className='p-3 h1text'>DASHBOARD</h1>
+                    </div>
+                <Outlet />
+                </div>
+            </div>
+            </div>
+
 	)
 }
 
