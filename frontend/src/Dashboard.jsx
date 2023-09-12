fbe7cb1b5d3c2cb59886fda4a8b21b01eaa6fbb7
Paul <johnpaul.c.edit@gmail.com>
2023-09-12 14:36:54 +0800

    sidebar paul

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -30,41 +30,6 @@ function Dashboard() {
 		}).catch(err => console.log(err));
 	}
 	return (
-		// <div className="container-fluid">
-		// 	<div className="row flex-nowrap">
-		// 		<div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-		// 			<div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-		// 				<a href="/" className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none">
-		// 					<span className="fs-5 fw-bolder d-none d-sm-inline">Admin Dashboard</span>
-		// 				</a>
-		// 				<ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-		// 					<li>
-		// 						<Link to="/" data-bs-toggle="collapse" className="nav-link text-white px-0 align-middle">
-		// 							<i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </Link>
-		// 					</li>
-		// 					<li>
-		// 						<Link to="/employee" className="nav-link px-0 align-middle text-white">
-		// 							<i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Employees</span> </Link>
-		// 					</li>
-		// 					{/* <li>
-		// 						<Link to="profile" className="nav-link px-0 align-middle text-white">
-		// 							<i className="fs-4 bi-person"></i> <span className="ms-1 d-none d-sm-inline">Profile</span></Link>
-		// 					</li> */}
-		// 					<li onClick={handleLogout}>
-		// 						<a href="#" className="nav-link px-0 align-middle text-white">
-		// 							<i className="fs-4 bi-power"></i> <span className="ms-1 d-none d-sm-inline">Logout</span></a>
-		// 					</li>
-		// 				</ul>
-		// 			</div>
-		// 		</div>
-		// 		<div class="col p-0 m-0">
-		// 			<div className='p-2 d-flex justify-content-center shadow'>
-		// 				<h4>Dashboard</h4>						
-		// 			</div>
-		// 			<Outlet />
-		// 		</div>
-		// 	</div>
-		// </div>
         <div className="container-fluid">
             <div className="row flex-nowrap">
                 <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 sidebar">
@@ -88,16 +53,14 @@ function Dashboard() {
                     {/* Your other navigation items */}
                     </ul>
                     {/* Move the logout button here */}
-                    <li onClick={handleLogout}>
-                    <a href="#" className="nav-link px-0 align-middle text-white">
+                    <a onClick={handleLogout} href="#" className="nav-link px-0 align-middle text-white">
                         <i className="fs-4 bi-power"></i> <span className="ms-1 d-none d-sm-inline">Logout</span>
                     </a>
-                    </li>
                 </div>
                 </div>
 
                 <div class="col p-0 m-0">
-                    <div className='p-2 d-flex justify-content-left shadow topbar'>
+                    <div className='p-2 d-flex justify-content-left shadow topbar banner'>
                     <h1 className='p-3 h1text'>DASHBOARD</h1>
                     </div>
                 <Outlet />
