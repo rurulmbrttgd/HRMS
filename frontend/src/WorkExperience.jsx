a3b595ed8e94a7c577b28fbb4297512a5479035a
Paul <johnpaul.c.edit@gmail.com>
2023-09-13 17:24:12 +0800

    workexpe

--- frontend/src/WorkExperience.jsx
+++ frontend/src/WorkExperience.jsx
@@ -55,129 +55,136 @@ function WorkExperienceForm() {
   const years = generateYearOptions(1990, 2030);
 
   return (
-    <div className='d-flex flex-column align-items-center pt-4'>
-      <h2>Work Experience</h2>
 
-      <form className="row g-3 w-50" onSubmit={handleSubmit}>
-        <div className="col-6">
-          <label htmlFor="from" className="form-label">From</label>
-          <select
-            className="form-select"
-            id="from"
-            name="from"
-            value={formData.from}
-            onChange={handleChange}
-          >
-            <option value="">Select Year</option>
-            {years.map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="col-6">
-          <label htmlFor="to" className="form-label">To</label>
-          <select
-            className="form-select"
-            id="to"
-            name="to"
-            value={formData.to}
-            onChange={handleChange}
-          >
-            <option value="">Select Year</option>
-            {years.map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="col-12">
-          <label htmlFor="positionTitle" className="form-label">Position Title</label>
-          <input
-            type="text"
-            className="form-control"
-            id="positionTitle"
-            name="positionTitle"
-            placeholder="Enter Position Title"
-            value={formData.positionTitle}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-12">
-          <label htmlFor="company" className="form-label">Company</label>
-          <input
-            type="text"
-            className="form-control"
-            id="company"
-            name="company"
-            placeholder="Enter Company"
-            autoComplete="off"
-            value={formData.company}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-6">
-          <label htmlFor="monthlySalary" className="form-label">Monthly Salary</label>
-          <input
-            type="text"
-            className="form-control"
-            id="monthlySalary"
-            name="monthlySalary"
-            placeholder="Enter Monthly Salary"
-            autoComplete="off"
-            value={formData.monthlySalary}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-6">
-          <label htmlFor="salaryIncrement" className="form-label">Salary Increment</label>
-          <input
-            type="text"
-            className="form-control"
-            id="salaryIncrement"
-            name="salaryIncrement"
-            placeholder="Enter Salary Increment"
-            autoComplete="off"
-            value={formData.salaryIncrement}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-6">
-          <label htmlFor="employmentStatus" className="form-label">Employment Status</label>
-          <input
-            type="text"
-            className="form-control"
-            id="employmentStatus"
-            name="employmentStatus"
-            placeholder="Enter Employment Status"
-            autoComplete="off"
-            value={formData.employmentStatus}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="col-6">
-          <label htmlFor="governmentService" className="form-label">Government Service</label>
-          <select
-            className="form-select"
-            id="governmentService"
-            name="governmentService"
-            value={formData.governmentService}
-            onChange={handleGovernmentServiceChange}
-          >
-            <option value="">Select</option>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-        </div>
-        <div className="col-12">
-          <button type="button" className="btn btn-primary" onClick={addWorkExperience}>
-            Add Another Field of Work Experience
-          </button>
+    <div className='d-flex flex-column align-items-left pt-4 shadow mainContainer'>
+      <h2>Work Experience</h2>
+      <hr></hr>
+      <form className="row g-3 w-100" onSubmit={handleSubmit}>
+        <div className="add-container">
+          <div className="d-flex flex-rows align-items-left first-row">
+            <div className="col-2 details">
+              <label htmlFor="from" className="form-label">From</label>
+              <select
+                className="form-select"
+                id="from"
+                name="from"
+                value={formData.from}
+                onChange={handleChange}
+              >
+                <option value="">From</option>
+                {years.map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col-2 details">
+              <label htmlFor="to" className="form-label">To</label>
+              <select
+                className="form-select"
+                id="to"
+                name="to"
+                value={formData.to}
+                onChange={handleChange}
+              >
+                <option value="">To</option>
+                {years.map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="col-4 details">
+              <label htmlFor="positionTitle" className="form-label">Position Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="positionTitle"
+                name="positionTitle"
+                placeholder="Position Title"
+                value={formData.positionTitle}
+                onChange={handleChange}/>
+            </div>
+            <div className="col-3 details">
+              <label htmlFor="company" className="form-label">Company</label>
+              <input
+                type="text"
+                className="form-control"
+                id="company"
+                name="company"
+                placeholder="Company"
+                autoComplete="off"
+                value={formData.company}
+                onChange={handleChange}/>
+            </div>
+          </div>
+          <div className="d-flex flex-rows align-items-left first-row">
+            <div className="col-3 details">
+              <label htmlFor="monthlySalary" className="form-label">Monthly Salary</label>
+              <input
+                type="text"
+                className="form-control"
+                id="monthlySalary"
+                name="monthlySalary"
+                placeholder="Monthly Salary"
+                autoComplete="off"
+                value={formData.monthlySalary}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-3 details">
+              <label htmlFor="salaryIncrement" className="form-label">Salary Increment</label>
+              <input
+                type="text"
+                className="form-control"
+                id="salaryIncrement"
+                name="salaryIncrement"
+                placeholder="00 - 0"
+                autoComplete="off"
+                value={formData.salaryIncrement}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-3 details">
+              <label htmlFor="employmentStatus" className="form-label">Status of Employment</label>
+              <input
+                type="text"
+                className="form-control"
+                id="employmentStatus"
+                name="employmentStatus"
+                placeholder="Status of Employment"
+                autoComplete="off"
+                value={formData.employmentStatus}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-2">
+              <label htmlFor="governmentService" className="form-label">Government Service</label>
+              <select
+                className="form-select"
+                id="governmentService"
+                name="governmentService"
+                value={formData.governmentService}
+                onChange={handleGovernmentServiceChange}
+              >
+                <option value="">Y/N</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </select>
+            </div>
+            
+          </div>
+          <div className="d-flex flex-rows align-items-left first-row">
+            <div className="col-12">
+                <button type="button" className="btn btn-primary" onClick={addWorkExperience}>
+                  Add Another Field of Work Experience
+                </button>
+            </div>
+          </div>
         </div>
       </form>
-
       {/* Display added work experiences */}
       <div className="mt-4">
         <h3>Added Work Experiences</h3>
