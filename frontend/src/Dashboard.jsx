876113b73098d968add184f2140a782d273126c5
rurulmbrttgd <ronaldlambertnicolastugadi@gmail.com>
2023-09-18 14:45:09 +0800

    fix class errors

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -59,7 +59,7 @@ function Dashboard() {
                 </div>
                 </div>
 
-                <div class="col p-0 m-0">
+                <div className="col p-0 m-0">
                     <div className='p-2 d-flex justify-content-left shadow topbar banner'>
                     <h1 className='p-3 h1text'>DASHBOARD</h1>
                     </div>
