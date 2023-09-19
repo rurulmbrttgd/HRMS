f105c503350e6e18716712b0e44f18bdc4be318e
Paul <johnpaul.c.edit@gmail.com>
2023-09-19 16:44:08 +0800

    employeedeets view mode

--- frontend/src/EmployeeDetails.jsx
+++ frontend/src/EmployeeDetails.jsx
@@ -36,17 +36,29 @@ function EmployeeDetails({ data }) {
             <label htmlFor="inputdateofbirth" className="form-label">Date of Birth:</label>
             <div>{data.dateOfBirth}</div>
           </div>
-          <div className="col-3 details">
+          <div className="col-2 details">
             <label htmlFor="inputplaceofbirth" className="form-label">Place of Birth:</label>
             <div>{data.placeOfBirth}</div>
           </div>
-          <div className="col-3 details">
+          <div className="col-2 details">
             <label htmlFor="inputcitizenship" className="form-label">Citizenship:</label>
             <div>{data.citizenship}</div>
           </div>
-          <div className="col-3 details">
+          <div className="col-2 details">
             <label htmlFor="inputstatus" className="form-label">Civil Status:</label>
             <div>{data.civilStatus}</div>
+          </div>
+          <div className="col-1 details">
+            <label htmlFor="inputheight" className="form-label">Height:</label>
+            <div>{data.height}</div>
+          </div>
+          <div className="col-1 details">
+            <label htmlFor="inputweight" className="form-label">Weight:</label>
+            <div>{data.weight}</div>
+          </div>
+          <div className="col-1 details">
+            <label htmlFor="inputbloodtype" className="form-label">Blood Type:</label>
+            <div>{data.bloodType}</div>
           </div>
         </div>
         
@@ -68,6 +80,103 @@ function EmployeeDetails({ data }) {
         
         {/* Residential Address */}
         <h6 className='residential'>RESIDENTIAL ADDRESS</h6>
+        <div className="d-flex flex-rows align-items-left first-row">
+          <div className="col details">
+            <label htmlFor="inputhouseNo" className="form-label">House/Block/Lot No:</label>
+            <div>{data.residentialAddress.houseNo}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputstreet" className="form-label">Street:</label>
+            <div>{data.residentialAddress.street}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputsubdivision" className="form-label">Subdivision/Village:</label>
+            <div>{data.residentialAddress.subdivision}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputbarangay" className="form-label">Barangay:</label>
+            <div>{data.residentialAddress.barangay}</div>
+          </div>
+        </div>
+
+        {/* City Column */}
+        <div className="d-flex flex-rows align-items-left first-row">
+          <div className="col details">
+            <label htmlFor="inputcity" className="form-label">City/Municipality:</label>
+            <div>{data.residentialAddress.city}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputprovince" className="form-label">Province:</label>
+            <div>{data.residentialAddress.province}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputzipCode" className="form-label">Zip Code:</label>
+            <div>{data.residentialAddress.zipCode}</div>
+          </div>
+        </div>
+
+        {/* Primary Address */}
+        <h6 className='residential'>PRIMARY ADDRESS</h6>
+        <div className="d-flex flex-rows align-items-left first-row">
+          <div className="col details">
+            <label htmlFor="inputhouseNo" className="form-label">House/Block/Lot No:</label>
+            <div>{data.primaryAddress.houseNo}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputstreet" className="form-label">Street:</label>
+            <div>{data.primaryAddress.street}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputsubdivision" className="form-label">Subdivision/Village:</label>
+            <div>{data.primaryAddress.subdivision}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputbarangay" className="form-label">Barangay:</label>
+            <div>{data.primaryAddress.barangay}</div>
+          </div>
+        </div>
+        {/* City Column */}
+        <div className="d-flex flex-rows align-items-left first-row">
+          <div className="col details">
+            <label htmlFor="inputcity" className="form-label">City/Municipality:</label>
+            <div>{data.primaryAddress.city}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputprovince" className="form-label">Province:</label>
+            <div>{data.primaryAddress.province}</div>
+          </div>
+          <div className="col details">
+            <label htmlFor="inputzipCode" className="form-label">Zip Code:</label>
+            <div>{data.primaryAddress.zipCode}</div>
+          </div>
+        </div>
+        {/* ID information */}
+        <div className="d-flex flex-rows align-items-left first-row">
+          <div className="col-1 details">
+            <label htmlFor="inputgsis" className="form-label">GSIS ID No:</label>
+            <div>{data.gsisIDNo}</div>
+          </div>
+          <div className="col-2 details">
+            <label htmlFor="inputpagibig" className="form-label">Pag-Ibig ID No:</label>
+            <div>{data.pagibigIDNo}</div>
+          </div>
+          <div className="col-2 details">
+            <label htmlFor="inputphilhealth" className="form-label">PhilHealth No:</label>
+            <div>{data.philhealthNo}</div>
+          </div>
+          <div className="col-2 details">
+            <label htmlFor="inputsss" className="form-label">SSS No:</label>
+            <div>{data.sssNo}</div>
+          </div>
+          <div className="col-2 details">
+            <label htmlFor="inputtin" className="form-label">TIN No:</label>
+            <div>{data.tinNo}</div>
+          </div>
+          <div className="col-2 details">
+              <label htmlFor="inputagency" className="form-label">Agency Employee No:</label>
+              <div>{data.agencyEmployeeNo}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
